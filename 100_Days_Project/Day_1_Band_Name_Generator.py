3e1b4b1d0064f400981f556ea682a4f8fd007e97
SynchDescron2025 <synchdescron2025@gmail.com>
2025-04-12 21:05:37 +0300

    1st day of my coding challenge, includes string concat and basic I/O operations.

---
+++ 100_Days_Project/Day_1_Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city_name = input("What city did you grew up in?")
+pet_name = input("What was the name of your pet?")
+print("Your band name could be "+ city_name + " " +pet_name)
