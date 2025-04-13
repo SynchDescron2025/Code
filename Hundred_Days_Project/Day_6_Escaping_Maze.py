5657e1540fbb6e6fcc57d43906a88a614ee65161
SynchDescron2025 <synchdescron2025@gmail.com>
2025-04-13 13:58:00 +0300

    Reeborg's World Maze Escape

---
+++ Hundred_Days_Project/Day_6_Escaping_Maze.py
@@ -0,0 +1,14 @@
+#Reeborg's World Maze escape
+"""
+while not at_goal():
+    if right_is_clear():
+        turn_left()
+        turn_left()
+        turn_left()
+    if front_is_clear():
+        move()
+    if not right_is_clear():
+        turn_left()
+"""
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
